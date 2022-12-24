724b39354a1b64100248661c938d6ef71a7b7d8d
jaeyjeon <jaeyjeon@student.42seoul.kr>
2022-12-24 16:07:53 +0900

    1

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -8,22 +8,26 @@ int	main( void )
 		int	vector_arr[] = {1, 2, 3, 3, 4, 5};
 		std::vector<int> vector_container( vector_arr, vector_arr + (sizeof(vector_arr)/sizeof(int)) );
 
-		try{ // search 3 & print form 3 to end
+		try
+		{ // search 3 & print 3 to end
 			std::vector<int>::iterator v_iter = easyfind(vector_container, 3);
 
 			while (v_iter != vector_container.end())
 				std::cout << *(v_iter++) << std::endl;
 		}
-		catch(const std::exception& e){
+		catch(const std::exception& e)
+		{
 			std::cerr << e.what() << std::endl;
 		}
-		try{ // search 400 & fail exception
+		try
+		{ // search 400 & fail exception
 			std::vector<int>::iterator v_iter = easyfind(vector_container, 400);
 
 			while (v_iter != vector_container.end())
 				std::cout << *(v_iter++) << std::endl;
 		}
-		catch (const std::exception& e){
+		catch (const std::exception& e)
+		{
 			std::cerr << e.what() << std::endl;
 		}
 	}
@@ -33,22 +37,26 @@ int	main( void )
 		int	list_arr[] = {200, -3, 1, 300, -40, -500};
 		std::list<int> list_container( list_arr, list_arr + (sizeof(list_arr)/sizeof(int)) );
 
-		try{ // search -3 & print form -3 to end
+		try
+		{ // search -3 & print form -3 to end
 			std::list<int>::iterator l_iter = easyfind< std::list<int> >(list_container, -3);
 
 			while (l_iter != list_container.end())
 				std::cout << *(l_iter++) << std::endl;
 		}
-		catch(const std::exception& e){
+		catch(const std::exception& e)
+		{
 			std::cerr << e.what() << std::endl;
 		}
-		try{ // search 89 & fail exception
+		try
+		{ // search 89 & fail exception
 			std::list<int>::iterator l_iter = easyfind< std::list<int> >(list_container, 89);
 
 			while (l_iter != list_container.end())
 				std::cout << *(l_iter++) << std::endl;
 		}
-		catch(const std::exception& e){
+		catch(const std::exception& e)
+		{
 			std::cerr << e.what() << std::endl;
 		}
 	}
@@ -58,22 +66,26 @@ int	main( void )
 		int	deque_arr[] = {5201, 326, -1, 330, 488, -503};
 		std::deque<int> deque_container( deque_arr, deque_arr + (sizeof(deque_arr)/sizeof(int)) );
 
-		try{ // search 5201 & print form 5201 to end
+		try
+		{ // search 5201 & print form 5201 to end
 			std::deque<int>::iterator d_iter = easyfind< std::deque<int> >(deque_container, 5201);
 
 			while (d_iter != deque_container.end())
 				std::cout << *(d_iter++) << std::endl;
 		}
-		catch(const std::exception& e){
+		catch(const std::exception& e)
+		{
 			std::cerr << e.what() << std::endl;
 		}
-		try{ // search -3 & fail exception
+		try
+		{ // search -3 & fail exception
 			std::deque<int>::iterator d_iter = easyfind< std::deque<int> >(deque_container, -3);
 
 			while (d_iter != deque_container.end())
 				std::cout << *(d_iter++) << std::endl;
 		}
-		catch(const std::exception& e){
+		catch(const std::exception& e)
+		{
 			std::cerr << e.what() << std::endl;
 		}
 	}
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,5 +1,68 @@
-#include ".hpp"
+#include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;  // 17
+	mstack.pop(); // -17
+	std::cout << mstack.size() << std::endl; // 1
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737); //[...] mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it; // -> 3
+	--it; // -> 5
+	while (it != ite)
+	{
+		std::cout << *it << std::endl; // 5 3 5 737
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout << s.top() << std::endl;
+	std::cout << s.size() << std::endl;
+	std::cout << "-----------------------------------------------" << std::endl;
+
+	MutantStack<char> my_stack;
+
+	my_stack.push('a');
+	my_stack.push('b');
+	my_stack.push('c');
+
+	MutantStack<char>::iterator my_iter_begin = my_stack.begin();
+	MutantStack<char>::iterator my_iter_end = my_stack.end();
+
+	std::list<char> my_list(my_iter_begin, my_iter_end);
+	std::list<char>::iterator my_list_begin = my_list.begin();
+	std::list<char>::iterator my_list_end = my_list.end();
+
+	for (; my_iter_begin != my_iter_end; my_iter_begin++)
+	{
+		std::cout << "mutantstack : " << *my_iter_begin << std::endl;
+	}
+
+	for (; my_list_begin != my_list_end; my_list_begin++)
+	{
+		std::cout << "list : " << *my_list_begin << std::endl;
+	}
+	std::cout << "----------reverse-----------------" << std::endl;
+	MutantStack<char>::reverse_iterator my_iter_rbegin = my_stack.rbegin();
+	MutantStack<char>::reverse_iterator my_iter_rend = my_stack.rend();
+
+	std::list<char>::reverse_iterator my_list_rbegin = my_list.rbegin();
+	std::list<char>::reverse_iterator my_list_rend = my_list.rend();
+
+	for (; my_iter_rbegin != my_iter_rend; my_iter_rbegin++)
+	{
+		std::cout << "mutantstack : "<< *my_iter_rbegin << std::endl;
+	}
+
+	for (; my_list_rbegin != my_list_rend; my_list_rbegin++)
+	{
+		std::cout << "list : "<< *my_list_rbegin << std::endl;
+	}
+	return 0;
 }
